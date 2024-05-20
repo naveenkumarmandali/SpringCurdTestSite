b88b28341d4b24db3e98fe30627aaa42a39d3125
naveen <naveen.mandali7@gmail.com>
2024-05-20 12:41:40 +0530

    employee controller soft delete api created.

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,16 +1,25 @@
 package com.example.controller;
 
-import com.example.dao.Employee;
-import com.example.services.EmployeeService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.dao.Employee;
+import com.example.services.EmployeeService;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -50,6 +59,12 @@ public class EmployeeController {
 	@PostMapping("/save_employee")
 	public ResponseEntity<Employee> saveData(@RequestBody Employee emp) {
 		return new ResponseEntity<>(empserv.saveEmployeeData(emp), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> softDelete(@PathVariable long id){
+		System.out.println("Soft Delete id: "+id);
+		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
 	}
 
 }
